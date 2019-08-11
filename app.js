5d6be95b3ad7f3fddac56726668b16487a3a58bb
liuyuyi <339266478@qq.com>
2019-08-11 11:56:04 +0800

    20190811

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const mongoose = require('./db/mongooseDb')
+// const mongoose = require('./db/mongooseDb')
 const express = require('express');
 const app = express();
 const https = require('https');
@@ -9,7 +9,7 @@ const nodemailer = require('nodemailer');
 const schedule = require('node-schedule');
 const targetUrl = 'https://m.cnal.com/market/changjiang/';
 // 截取网页生成图
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
 // 创建一个SMTP客户端配置
 const config = {
     host: 'smtp.163.com', //网易163邮箱 smtp.163.com
@@ -30,8 +30,28 @@ var mail = {
     // 收件人
     to: '339266478@qq.com,328826649@qq.com',
     // 邮件内容，HTML格式
-    html: '<table class="table" style="border: 1px solid rgb(153, 153, 153);"><tbody><tr><td>序号</td> <td>规格</td> <td>加工费<br>CCAQA-1/155</td> <td>当日铜价</td> <td>当日铝价</td> <td>当日含税价单价</td> <td>当日不含税单价</td></tr> <tr class="item"><td>1</td> <td style="white-space: nowrap;">0.10-0.11</td> <td>26.16</td> <td rowspan="10" style="background: rgb(161, 216, 252);">46.7</td> <td rowspan="10" style="background: rgb(197, 161, 252);">13.94</td> <td>49.93</td> <td>46.23</td></tr><tr class="item"><td>2</td> <td style="white-space: nowrap;">0.12-0.13</td> <td>24.11</td> <td rowspan="10" style="background: rgb(161, 216, 252); display: none;">46.7</td> <td rowspan="10" style="background: rgb(197, 161, 252); display: none;">13.94</td> <td>47.88</td> <td>44.33</td></tr><tr class="item"><td>3</td> <td style="white-space: nowrap;">0.14-0.15</td> <td>18.56</td> <td rowspan="10" style="background: rgb(161, 216, 252); display: none;">46.7</td> <td rowspan="10" style="background: rgb(197, 161, 252); display: none;">13.94</td> <td>42.33</td> <td>39.19</td></tr><tr class="item"><td>4</td> <td style="white-space: nowrap;">0.16-0.17</td> <td>17.31</td> <td rowspan="10" style="background: rgb(161, 216, 252); display: none;">46.7</td> <td rowspan="10" style="background: rgb(197, 161, 252); display: none;">13.94</td> <td>41.08</td> <td>38.04</td></tr><tr class="item"><td>6</td> <td style="white-space: nowrap;">0.18-0.19</td> <td>16.76</td> <td rowspan="10" style="background: rgb(161, 216, 252); display: none;">46.7</td> <td rowspan="10" style="background: rgb(197, 161, 252); display: none;">13.94</td> <td>40.53</td> <td>37.53</td></tr><tr class="item"><td>7</td> <td style="white-space: nowrap;">0.20-0.24</td> <td>13.83</td> <td rowspan="10" style="background: rgb(161, 216, 252); display: none;">46.7</td> <td rowspan="10" style="background: rgb(197, 161, 252); display: none;">13.94</td> <td>37.60</td> <td>34.81</td></tr><tr class="item"><td>8</td> <td style="white-space: nowrap;">0.25-0.29</td> <td>13.33</td> <td rowspan="10" style="background: rgb(161, 216, 252); display: none;">46.7</td> <td rowspan="10" style="background: rgb(197, 161, 252); display: none;">13.94</td> <td>37.10</td> <td>34.35</td></tr><tr class="item"><td>9</td> <td style="white-space: nowrap;">0.30-0.39</td> <td>13.26</td> <td rowspan="10" style="background: rgb(161, 216, 252); display: none;">46.7</td> <td rowspan="10" style="background: rgb(197, 161, 252); display: none;">13.94</td> <td>37.03</td> <td>34.29</td></tr><tr class="item"><td>10</td> <td style="white-space: nowrap;">0.40-0.49</td> <td>12.8</td> <td rowspan="10" style="background: rgb(161, 216, 252); display: none;">46.7</td> <td rowspan="10" style="background: rgb(197, 161, 252); display: none;">13.94</td> <td>36.57</td> <td>33.86</td></tr><tr class="item"><td>11</td> <td style="white-space: nowrap;">0.50-1.30</td> <td>12.4</td> <td rowspan="10" style="background: rgb(161, 216, 252); display: none;">46.7</td> <td rowspan="10" style="background: rgb(197, 161, 252); display: none;">13.94</td> <td>36.17</td> <td>33.49</td></tr></tbody></table>' //可以是链接，也可以是验证码
+    html: ''
 };
+var listData = [{id: 1, size: '0.10-0.11', addMuch: 26.16},
+{ id: 2, size: '0.12-0.13', addMuch: 24.11 },
+{ id: 3, size: '0.14-0.15', addMuch: 18.56 },
+{ id: 4, size: '0.16-0.17', addMuch: 17.31 },
+{ id: 6, size: '0.18-0.19', addMuch: 16.76 },
+{ id: 7, size: '0.20-0.24', addMuch: 13.83 },
+{ id: 8, size: '0.25-0.29', addMuch: 13.33 },
+{ id: 9, size: '0.30-0.39', addMuch: 13.26 },
+{ id: 10, size: '0.40-0.49', addMuch: 12.80 },
+{ id: 11, size: '0.50-1.30', addMuch: 12.40 }];
+var shtml = '<table class="table" style="border: 1px solid #999999;">\
+<tr>\
+    <td style="	padding: 6px 3px;text-align: center;border: 1px solid #999999;">序号</td>\
+    <td style="	padding: 6px 3px;text-align: center;border: 1px solid #999999;">规格</td>\
+    <td style="	padding: 6px 3px;text-align: center;border: 1px solid #999999;">加工费<br/>CCAQA-1/155</td>\
+    <td style="	padding: 6px 3px;text-align: center;border: 1px solid #999999;">当日铜价</td>\
+    <td style="	padding: 6px 3px;text-align: center;border: 1px solid #999999;">当日铝价</td>\
+    <td style="	padding: 6px 3px;text-align: center;border: 1px solid #999999;">当日含税价单价</td>\
+    <td style="	padding: 6px 3px;text-align: center;border: 1px solid #999999;">当日不含税单价</td>\
+</tr>';
 /*  数据库连接  */
 const PriceSchema = {
     toPrice: { // 铜
@@ -43,22 +63,22 @@ const PriceSchema = {
     upDateTime: { type: String },
     creatDate: { type: Number }
 }
-const PriceModel = mongoose.model('newprice', PriceSchema);
+// const PriceModel = mongoose.model('newprice', PriceSchema);
 const Rule2 = new schedule.RecurrenceRule();
-    Rule2.hour = [16];
-    Rule2.minute = [32];
+    Rule2.hour = [09];
+    Rule2.minute = [00];
 
 schedule.scheduleJob(Rule2,  () =>{
 
-    (async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(targetUrl);
-        await page.screenshot({
-            path: 'example.jpg'
-        });
-        await browser.close();
-    })();
+    // (async () => {
+    //     const browser = await puppeteer.launch();
+    //     const page = await browser.newPage();
+    //     await page.goto(targetUrl);
+    //     await page.screenshot({
+    //         path: 'example.jpg'
+    //     });
+    //     await browser.close();
+    // })();
 
     https.get(targetUrl, (res) => {
 
@@ -84,28 +104,51 @@ schedule.scheduleJob(Rule2,  () =>{
                     $parent = $($('.cnal-market-table td')[i]).parent();
                 if (type === '铝') {
                     console.log('铝' + $($parent.find('td')[2]).text())
-                    priceData.lvPrice = $($parent.find('td')[2]).text()*1;
+                    priceData.lvPrice = $($parent.find('td')[2]).text()*1/1000;
                     priceData.upDateTime = $($parent.find('td')[4]).text();
                 } else if (type === '铜') {
                     console.log('铜' + $($parent.find('td')[2]).text())
-                    priceData.toPrice = $($parent.find('td')[2]).text() * 1;
+                    priceData.toPrice = $($parent.find('td')[2]).text() * 1/1000;
                 }
             }
-            
-            PriceModel.findOne({
-                upDateTime: priceData.upDateTime
-            }, function (err, doc) {
+
+            for(var e=0,elen=listData.length;e<elen;e++){
+
+                var item = listData[e],
+                    colHtml = '<td rowspan="10" style="background: #a1d8fc;">'+priceData.toPrice+'</td>\
+                    <td rowspan="10" style="background: #c5a1fc;">'+ priceData.lvPrice +'</td>';
+
+                shtml += '<tr v-for="(item, index) in listData" :key="item.id" class="item">\
+                            <td style="	padding: 6px 3px;text-align: center;border: 1px solid #999999;">'+ item.id + '</td>\
+                            <td style="white-space: nowrap;	padding: 6px 3px;text-align: center;border: 1px solid #999999;">'+item.size +'</td>\
+                            <td style="	padding: 6px 3px;text-align: center;border: 1px solid #999999;">'+ item.addMuch + '</td>\
+                            '+ (e === 0 ? colHtml : '') +'\
+                            <td style="	padding: 6px 3px;text-align: center;border: 1px solid #999999;">'+ ((priceData.toPrice*0.3)+(priceData.lvPrice*0.7)+item.addMuch).toFixed(2) +'</td>\
+                            <td style="	padding: 6px 3px;text-align: center;border: 1px solid #999999;">'+ (((priceData.toPrice*0.3)+(priceData.lvPrice*0.7)+item.addMuch)/1.08).toFixed(2) +'</td>\
+                        </tr>'
+
+            }
+            shtml += '</table>';
+
+            mail.html = shtml;
+            mail.subject = priceData.upDateTime + '铜铝价格';
+
+            send(mail);
+
+            // PriceModel.findOne({
+            //     upDateTime: priceData.upDateTime
+            // }, function (err, doc) {
                 
-                if(doc === null){
+            //     if(doc === null){
 
-                    var price = new PriceModel(priceData);
-                    price.save();
+            //         var price = new PriceModel(priceData);
+            //         price.save();
 
-                }else{
-                    console.log('已经存在不保存')
-                }
+            //     }else{
+            //         console.log('已经存在不保存')
+            //     }
 
-            });
+            // });
 
         });
 
@@ -124,7 +167,6 @@ function send(mail) {
         console.log('mail sent:', info.response);
     });
 };
-send(mail);
 
 app.use('/public', express.static('public'));
 app.get('/', (req, res) => {
