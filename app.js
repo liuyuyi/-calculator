fe1fcbd0087f2784b1031b60c1b5efb661469815
liuyuyi <339266478@qq.com>
2024-03-02 10:21:23 +0800

    feat: 1

--- app.js
+++ app.js
@@ -139,7 +139,17 @@ Rule2.minute = [00, 30];
 // schedule.scheduleJob(Rule2, () => {
     (async () => {
         const browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+            headless: true,
+            args: [ '--disable-gpu',
+                '--disable-dev-shm-usage',
+                '--disable-setuid-sandbox',
+                '--no-first-run',
+                '--no-sandbox',
+                '--no-zygote',
+                '--deterministic-fetch',
+                '--disable-features=IsolateOrigins',
+                '--disable-site-isolation-trials'
+            ]
         });
         const page = await browser.newPage();
         await page.goto(targetUrl, { timeout: 10000*60, waitUntil: 'networkidle2' });
